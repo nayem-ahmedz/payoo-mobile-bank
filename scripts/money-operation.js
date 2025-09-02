11f9499fb09566bfa9172e5d2daf9f1dbc6e1b2d
nayem-ahmedz <nayemahmed2n@gmail.com>
2025-09-02 22:39:45 +0600

    optimize codes using more functions and added Transfer Money feaures, and more validation method is added

--- scripts/money-operation.js
+++ scripts/money-operation.js
@@ -1,51 +1,114 @@
 // default pincode
 const userPinCode = 1234;
 
+// add money system
 document.getElementById('add-money-button').addEventListener('click', function(event){
     event.preventDefault();
-    const bankName = getInputValue('bank-name');
+    const bankName = getInputValue('bank-name'); // simply get the value
     const accountNumber = getInputValue('account-number');
-    const amount = getInputNumber('amount');
+    const amount = getInputNumber('amount'); // get the value in number
     const pinCode = getInputNumber('pincode');
-    
-    if(bankName === 'select' || !accountNumber || !amount || !pinCode){
-        alert('Please fill in the boxs with valid input');
+    if(bankName === 'select'){
+        alert('Select a bank');
         return;
     }
-    if(accountNumber.length < 11){
-        alert('Enter 11 Digit Account number');
-        return;
+    if(validateInputs([accountNumber, amount, pinCode])){
+        if(validateAccount(accountNumber) && validateAmount(amount) && validatePin(pinCode)){
+            const availableBal = Number(getInnerText('available-balance')); // get available bal / DOM
+            const updatedAvBal = availableBal + amount;
+            setInnerText('available-balance', updatedAvBal); // set updated balance in DOM
+            getElement('add-money-form').reset(); // resetting the form after succesfull operation
+            alert(`Add Money $${amount} is succesfull, current balance is $${updatedAvBal}`);
+        }
     }
-    if(pinCode !== userPinCode){
-        alert('Invalid Pin');
-        return;
-    }
-    const availableBal = getInnerText('available-balance');
-    let updatedAvBal = Number(availableBal) + amount;
-    setInnerText('available-balance', updatedAvBal);
 });
 
+// withdraw system
 document.getElementById('withdraw-button').addEventListener('click', function(e){
     e.preventDefault();
     const agentNumber = getInputValue('agent-number');
     const amount = getInputNumber('withdraw-amount');
-    const pinCode = getInputNumber('pincode2');
-    if(!agentNumber || !amount || !pinCode){
-        alert('Please fill in the boxs with valid input');
-        return;
+    const pin = getInputNumber('pincode2');
+    if(validateInputs([agentNumber, amount, pin])){
+        if(validateAccount(agentNumber) && validateAmount(amount) && validatePin(pin)){
+            const availableBal = Number(getInnerText('available-balance'));
+            if(availableBal >= amount){
+                const updatedAvBal = availableBal - amount;
+                setInnerText('available-balance', updatedAvBal);
+                getElement('cash-out-form').reset();
+                alert(`Withdraw $${amount} is successful`);
+            } else{
+                alert('Insufficient Balance!');
+            }
+        }
     }
-    if(agentNumber.length < 11){
-        alert('Enter 11 digit Agent Number');
-        return;
-    }
-    if(pinCode !== userPinCode){
-        alert('Invalid Pin');
-        return;
-    }
-    const availableBal = getInnerText('available-balance');
-    let updatedAvBal = Number(availableBal) - amount;
-    setInnerText('available-balance', updatedAvBal);
 });
+
+// Transfer Money System
+document.getElementById('transfer-button').addEventListener('click', function(event){
+    event.preventDefault();
+    const transferNumber = getInputValue('transfer-number');
+    const amount = getInputNumber('transfer-amount');
+    const pinCode = getInputNumber('pincode3');
+    if(validateInputs([transferNumber, amount, pinCode])){
+        if(validateAccount(transferNumber) && validateAmount(amount) && validatePin(pinCode)){
+            const availableBal = Number(getInnerText('available-balance'));
+            if(availableBal >= amount){
+                const updatedAvBal = availableBal - amount;
+                setInnerText('available-balance', updatedAvBal);
+                getElement('transfer-money-form').reset();
+                alert(`Amount $${amount} is succesfully Transferred to account no ${transferNumber}`);
+            } else{
+                alert('Insufficient Balance!');
+            }
+        }
+    }
+});
+
+// validate input
+function validateInputs(inputs){
+    for(const input of inputs){
+        if(!input){
+            alert('Please fill in the boxs with valid input ');
+            return false;
+        }
+    }
+    return true;
+}
+
+// validate account nubmer
+function validateAccount(number){
+    if(number.length < 11){
+        alert('Enter 11 digit Agent Number')
+        return false;
+    }
+    for(const digit of number){
+        if(digit < '0' || digit > '9'){
+            alert('account number should be numeric');
+            return false;
+        }
+    }
+    return true;
+}
+
+// validate amount
+function validateAmount(amount){
+    if(amount < 0){
+        alert('negative amount is not allowed');
+        return false;
+    }
+    return true;
+}
+
+// validate pin
+function validatePin(pin){
+    const userPinCode = 1234;
+    if(pin === userPinCode){
+        return true;
+    }
+    alert('Invalid Pin');
+    return false;
+}
 
 // function to get element, input value and input value in Number
 function getElement(id){
@@ -59,7 +122,7 @@ function getInputValue(id){
 function getInputNumber(id){
     const value = getInputValue(id);
     const numberValue = Number(value);
-    return isNaN(numberValue) ? null : numberValue;
+    return isNaN(numberValue) ? -1 : numberValue;
 }
 
 // function to set and get Inner Text
